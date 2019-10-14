51ecbca5ab3e54c539b2ce3dad5373cc5c26f3b2
aki810 <takaaki.kobayashi.810.sg@gmail.com>
2019-10-14 13:54:18 +0900

    Update mjasp.js

--- mjasp.js
+++ mjasp.js
@@ -1,3 +1,17 @@
-window.onload = function test() {
-  alert("Hello!");
+window.onload = function writeCoolie(){
+  document.cookie = 'mjasp_adcode=' + getParam('adcode');
+}
+
+function getAcodeFromCookie() { 
+  var r = document.cookie.indexOf('mjasp_adcode');
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
